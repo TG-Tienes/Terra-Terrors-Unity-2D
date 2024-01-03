746e559cf4cc35ad3741435a0de177adc6a2ccb5
ptminngoc <pngoc0105@gmail.com>
2024-01-03 21:21:15 +0700

    fix quest again

--- Assets/Scripts/Quest/QuestManager.cs
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -11,7 +11,6 @@ public class QuestManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(this);
             Reset();
         }
         else
